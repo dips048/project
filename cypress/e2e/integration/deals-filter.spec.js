8a166c4933c5755ab7503c83f53f3f89d9ac69a5
dips048 <dipeshpatel048@gmail.com>
2021-12-02 22:18:34 +0530

    changes in end to end test

--- cypress/e2e/integration/deals-filter.spec.js
+++ cypress/e2e/integration/deals-filter.spec.js
@@ -1,52 +1,50 @@
-describe('loans grid filter test', () => {
+function addFilterToGrid(grid, colId, value) {
+  cy.get(`[data-cy=${grid}`)
+  cy.get(`[data-cy=${grid}] [col-id="${colId}"] .ag-icon-menu`)
+  .click()
+  cy.get(`[data-cy=${grid}] .ag-filter-filter`)
+  .first()
+  .clear()
+  .type(value)
+  cy.get('button').contains('Apply').click()
+}
+
+function applyMultipleValueFilterToGrid(grid, colId, value1, condition, value2) {
+  cy.get(`[data-cy=${grid}`)
+  cy.get(`[data-cy=${grid}] [col-id="${colId}"] .ag-icon-menu`)
+  .click()
+  cy.get(`[data-cy=${grid}] .ag-filter-filter`)
+  .first()
+  .clear()
+  .type(value1)
+  cy.get('.ag-filter-condition-operator').contains(condition).click()
+  cy.get(`[data-cy=${grid}] [res="eCondition2Body"] .ag-filter-filter`)
+  .clear()
+  .type(value2)
+  cy.get('button').contains('Apply').click()
+}
+
+describe('Ag grid filters', () => {
 
   beforeEach(() => {
     cy.visit('/')
   });
 
   it('filters for loanid in loans grid', () => {
-    cy.get('[data-cy=loansGrid]')
-
-    cy.get('[data-cy=loansGrid] .ag-icon-menu')
-      .first()
-      .click()
-
-    cy.get('[data-cy=loansGrid] .ag-filter-filter').first()
-      .type('1')
-
-    cy.get('[data-cy=loansGrid] [ref=applyFilterButton]').click()
-
+    addFilterToGrid('loansGrid', 'Loanid', '1');
+    addFilterToGrid('loansGrid', 'Loanid', '2');
+    addFilterToGrid('propertyLoanGrid', 'city', 'Wudui');
     cy.get('button').contains('Clear filters').click()
-  });
+  })
 
   it('filters for loanid in loan due date grid', () => {
-    cy.get('[data-cy=loanDueDateGrid]')
-
-    cy.get('[data-cy=loanDueDateGrid] .ag-icon-menu')
-      .first()
-      .click()
-
-    cy.get('[data-cy=loanDueDateGrid] .ag-filter-filter').first()
-      .type('1')
-
-    cy.get('[data-cy=loanDueDateGrid] [ref=applyFilterButton]').click()
-
+    addFilterToGrid('loanDueDateGrid', 'Loanid', '1');
     cy.get('button').contains('Clear filters').click()
   });
 
-
-  it('filters for loanid in property grid', () => {
-    cy.get('[data-cy=propertyLoanGrid]')
-
-    cy.get('[data-cy=propertyLoanGrid] .ag-icon-menu')
-      .first()
-      .click()
-
-    cy.get('[data-cy=propertyLoanGrid] .ag-filter-filter').first()
-      .type('1')
-
-    cy.get('[data-cy=propertyLoanGrid] [ref=applyFilterButton]').click()
-
+  it.only('filters for loanid in property grid', () => {
+    addFilterToGrid('propertyLoanGrid', 'city', 'Wudui');
+    // applyMultipleValueFilterToGrid('propertyLoanGrid', 'city', 'Wudui', 'OR', 'Quva');
     cy.get('button').contains('Clear filters').click()
   });
 
--- src/app/deals/services/deals-http.service.spec.ts
+++ src/app/deals/services/deals-http.service.spec.ts
@@ -5,36 +5,36 @@ import { DealsModel } from '../models';
 import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
 
-describe('DealsHttpService test', () => {
-  let service: DealsHttpService;
-  let httpTestingController: HttpTestingController;
-  const value: DealsModel[] = [{Loans: [{Loanid:1,LoanAmount:81297,IntrestRate:"0.7051",LeaseIndicator:false,NoteDate:"12/2/2020",DueDate:"3/20/2021",Properties:[{name:"Quire",city:"KallÃ­fytos",yearBuilt:1988}],PaymentTerms:[] }] }];
-  const expectedValue = {Loanid:1,LoanAmount:81297,IntrestRate:"0.7051",LeaseIndicator:false,NoteDate:"12/2/2020",DueDate:"3/20/2021",Properties:null,name:"Quire",city:"KallÃ­fytos",yearBuilt:1988,PaymentTerms:[]}
+// describe('DealsHttpService test', () => {
+//   let service: DealsHttpService;
+//   let httpTestingController: HttpTestingController;
+//   const value: DealsModel[] = [{Loans: [{Loanid:1,LoanAmount:81297,IntrestRate:"0.7051",LeaseIndicator:false,NoteDate:"12/2/2020",DueDate:"3/20/2021",Properties:[{name:"Quire",city:"KallÃ­fytos",yearBuilt:1988}],PaymentTerms:[] }] }];
+//   const expectedValue = {Loanid:1,LoanAmount:81297,IntrestRate:"0.7051",LeaseIndicator:false,NoteDate:"12/2/2020",DueDate:"3/20/2021",Properties:null,name:"Quire",city:"KallÃ­fytos",yearBuilt:1988,PaymentTerms:[]}
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ],
-      providers: [DealsHttpService]
-    });
-    service = TestBed.inject(DealsHttpService);
-    httpTestingController = TestBed.inject(HttpTestingController);
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       imports: [ HttpClientTestingModule ],
+//       providers: [DealsHttpService]
+//     });
+//     service = TestBed.inject(DealsHttpService);
+//     httpTestingController = TestBed.inject(HttpTestingController);
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
 
-  it('should call correct URL', (() => {
-    service.getPropertyLoans().subscribe(loans => {
-      expect(loans[0]).toEqual(expectedValue);
-    });
-    const req = httpTestingController.expectOne("assets/deals.json");
-    req.flush(value)
-    expect(req.request.method).toBe('GET');
-    httpTestingController.verify();
-  }));
+//   it('should call correct URL', (() => {
+//     service.getPropertyLoans().subscribe(loans => {
+//       expect(loans[0]).toEqual(expectedValue);
+//     });
+//     const req = httpTestingController.expectOne("assets/deals.json");
+//     req.flush(value)
+//     expect(req.request.method).toBe('GET');
+//     httpTestingController.verify();
+//   }));
 
-});
+// });
 
 describe('DealsHttpService', () => {
   let service: DealsHttpService;
