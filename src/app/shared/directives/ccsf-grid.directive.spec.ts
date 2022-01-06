9f5a375a7e87f7d1cfda6fd7ae8481ae5dec5e72
dips048 <dipeshpatel048@gmail.com>
2022-01-06 23:47:32 +0530

    tried some css changes on ag grid

--- src/app/shared/directives/ccsf-grid.directive.spec.ts
+++ src/app/shared/directives/ccsf-grid.directive.spec.ts
@@ -1,73 +1,57 @@
-import { Component, DebugElement } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { AgGridModule } from 'ag-grid-angular';
+import { PropertyLoanGridComponent } from 'src/app/deals/components/property-loan-grid/property-loan-grid.component';
+import { QaGlobalsService } from '../services';
 import { CcsfGridDirective } from './ccsf-grid.directive';
 
-@Component({
-  template: `
-  <h2 highlight="skyblue">About</h2>
-  <h3>Quote of the day:</h3>
-  `
-})
-export class TestComponent { }
+// @Component({
+//   template: `
+//     <ag-grid-angular id="loanDueDateGrid" [ccsfGrid]="'loanDueDateGrid'" data-cy="loanDueDateGrid" #myGrid (filterChanged)="onFilterChanged()"
+//         style="width: 600px; height: 500px;"
+//         class="ag-theme-alpine"
+//         [rowData]="rowData"
+//         [gridOptions]="gridOptions">
+//     </ag-grid-angular>
+//   `
+// })
+// export class TestComponent { }
 
 describe('CcsfGridDirective', () => {
-  let fixture: ComponentFixture<TestComponent>;;
-  let des: DebugElement;
+  let component: PropertyLoanGridComponent;
+  let fixture: ComponentFixture<PropertyLoanGridComponent>;;
+  let service: QaGlobalsService;
+  let directive: CcsfGridDirective;
 
   beforeEach(() => {
     fixture = TestBed.configureTestingModule({
-      declarations: [ CcsfGridDirective, TestComponent ]
+      declarations: [ CcsfGridDirective, PropertyLoanGridComponent ],
+      providers: [QaGlobalsService],
+      imports: [
+        AgGridModule
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .createComponent(TestComponent);
-    fixture.detectChanges(); // initial binding
-  
-    // all elements with an attached CcsfGridDirective
-    des = fixture.debugElement.query(By.directive(CcsfGridDirective));
-  
+    .createComponent(PropertyLoanGridComponent);
   });
-  
-  // it('should create an instance', () => {
-  //   const directive = new CcsfGridDirective();
-  //   expect(directive).toBeTruthy();
-  // });
 
-  // color tests
-  // it('should have three highlighted elements', () => {
-  //   expect(des).toBe(3);
-  // });
-  
-  it('should color 1st <h2> background "yellow"', () => {
-    const bgColor = des.nativeElement.api;
-    expect(bgColor).toBe('yellow');
-  });
-  
-  // it('should color 2nd <h2> background w/ default color', () => {
-  //   const dir = des[1].injector.get(CcsfGridDirective) as CcsfGridDirective;
-  //   const bgColor = des[1].nativeElement.style.backgroundColor;
-  //   expect(bgColor).toBe(dir.defaultColor);
-  // });
-  
-  it('should bind <input> background to value color', () => {
-    // easier to work with nativeElement
-    const input = des[2].nativeElement as HTMLInputElement;
-    expect(input.style.backgroundColor).toBe('cyan', 'initial backgroundColor');
-  
-    input.value = 'green';
-  
-    // Dispatch a DOM event so that Angular responds to the input value change.
-    // In older browsers, such as IE, you might need a CustomEvent instead. See
-    // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
-    input.dispatchEvent(new Event('input'));
+  beforeEach(() => {
+    component = fixture.componentInstance;
+    service = TestBed.inject(QaGlobalsService);
     fixture.detectChanges();
-  
-    expect(input.style.backgroundColor).toBe('green', 'changed backgroundColor');
+    directive = new CcsfGridDirective(component.myGrid, service);
+  })
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
   });
-  
-  
-  // it('bare <h2> should not have a customProperty', () => {
-  //   expect(bareH2.properties['customProperty']).toBeUndefined();
-  // });
+
+  it('should create an instance of directive', () => {
+    const dir = fixture.debugElement.query(By.directive(CcsfGridDirective));
+    expect(dir).toBeTruthy();
+    console.log(dir, window['QaGlobalsService']);
+  })
 });
 
 
